b5783abf22cb8f290bda86741092b82399b1e5e9
Drew <drewstuff5@hotmail.com>
2015-01-16 08:53:18 -0500

    Project 2

---
+++ js/Old and backup js/resumeBuilder-awesome.js
@@ -0,0 +1,7 @@
+$("#main").append("Andrew Totten");
+
+var awesomeThought = "Everything is Awesome";
+console.log(awesomeThought);
+
+var funThoughts = awesomeThought.replace("Awesome","Crazy");
+$("#main").append(funThoughts);
---
+++ js/resumeBuilder.js
@@ -0,0 +1,390 @@
+// resume biographical information
+var bio = {
+	"name":"Andrew Totten",
+	"role":"Web Developer",
+	"contacts": {
+		"mobile":"989-615-8981",
+		"email":"drewstuff5@hotmail.com",
+		"location":"Birch Run, MI",
+		"twitter": "@drewtott"
+	},
+	"welcomemsg":"My desire is to solve business and technical problems by developing user friendly applications and solutions.",
+	"skills": ["Analytical", "Intuitive", "Resourceful", "Adaptable", "Inovative","Quick Learner"],
+	"character": ["Integrity", "Respectful", "Patient", "Self-motivated"],
+	"bioPic": "images/drew.jpg"
+};
+
+// resume work experience information
+var work = {
+	"jobs": [
+		{
+			"employer": "AT&T",
+			"title": "Sr. Solutions Architect",
+			"location": "Saginaw, MI",
+			"dates": "March 1996 to Present",
+			"description": "Designed and solutioned voice transformation solutions for customers",
+			"experience": [
+				"Consulted customers and designed solutions to meet their business and technology needs and desires using Avaya and Cisco solutions",
+				"Assisted in AT&T process development for Avaya solutions",
+				"Managed engineering sub teams to develop large and complex solutions for customers",
+				"Assisted peers with complex designs and solutions",
+				"Provided training to engieners and sales teams on Avaya products and solutions",
+				"Studied Value Based Selling and the Science of Consultative Selling",
+				"Trained on various products including Avaya, Cisco, Juniper Networks, Adtran, and Shortel"
+			]
+		},
+		{
+			"employer": "AT&T",
+			"title": "Proposal, Management, and Implementation (PMI) Tool Developer",
+			"location": "Saginaw, MI",
+			"dates": "January 1997 to November 2001; August 2006 to Present",
+			"description": "Designed, developed, and supported an Excel based customer quote and implementation pricing tool",
+			"experience": [
+				"Evolved a basic Excel based tool into a complex cusotmer quoting application",
+				"Developed user friendly processes and inputs to address end user business needs",
+				"Created Excel visual basic macros and spreadsheets",
+				"Engaged product management to introduce new products into the tool",
+				"Led process improvement projects to improve the design to quote cycle time",
+				"Interfaced with AT&T Business Field Services to develop task times for implementation pricing",
+				"Provided training to engineers and sales teams on the use of the PMI Tool"
+			]
+		},
+		{
+			"employer": "Partner Community Council (PCC)",
+			"title": "Configuration Tools Task Force chair",
+			"location": "Saginaw, MI",
+			"dates": "September 2000 to December 2008; January 2010 to Present",
+			"description": "Lead, guided, and coordinated discussion and issue resolution between Nortel and Avaya partners and Avaya",
+			"experience": [
+				"Lead the direction and focus of the task force",
+				"Coordinated and lead the monthly conference calls as well as the conference breakout sessions",
+				"Analyized and evaluated Avaya tools and processes for designing and solutioning Avaya products and services",
+				"Acted as a liason between AT&T and the PCC to address concerns and issues",
+				"Participated in the Engineering, IP Convergence, Training, and Product Management Task Forces",
+				"Escalate and discussed critical issues with the PCC Board of Directors and Avaya Executives",
+				"Assisted Global Knowledge with the design of a Sales Engineering class"
+			]
+		},
+		{
+			"employer": "Nortel",
+			"title": "CAST Lab Engineer",
+			"location": "Plano, TX",
+			"dates": "June 1995 to March 1996",
+			"description": "Testing and evaluating Nortel products and solutions.",
+			"experience": [
+				"Configured and tested hardware and software features on Nortel Meridian 1 PBX systems",
+				"Assisted Nortel ETAS (*Emergency Technical Assistance Support) with the testing of issues",
+				"Assisted new product introduction engineers withe testing and standardization of Nortel products",
+				"Installed Macintosh and PC based computers for the lab and new product introduction engineers",
+				"Designed and implemented an inventory system for the CAST Lab"
+			]
+		},
+		{
+			"employer": "Nortel",
+			"title": "Software Systems Engineer",
+			"location": "Research Triangle Park, NC",
+			"dates": "June 1994 to December 1994",
+			"description": "Supported a SCO Unix based server used for telecommunication central office translations .",
+			"experience": [
+				"Installed and maintenance the Computer Operated Data Entry System (CODES) software",
+				"Supported the SCO Unix mainframed used for the CODES software",
+				"Installed, configured and maintaine a sucre multi-modem bank and digiboard system"
+			]
+		}
+	]
+};
+
+// resume project information
+var projects = {
+	"projects": [
+		{
+			"title": "Front-End Web Design Nanodegree Project 1 - HTML and CSS Design",
+			"dates": "November 2014",
+			"description": "Translation of a mockup into a web page viewable from a desktop and mobile browser:",
+			"images": "images/project1-600.png"
+		},
+		{
+			"title": "Front-End Web Design Nanodegree Project 2 - JavaScript Design",
+			"dates": "January 2015",
+			"description": "This interactive resume is the result of project 2.",
+			"images": ""
+		},
+		{	"title": "Proposal Management and Implementation Tool",
+			"dates": "January 2015",
+			"description": "Excel based, visual basic driven sales quote tool and implementation pricing design tool:",
+			"images": "images/pmi-600.png"
+		}
+	]
+};
+
+// resume schooling/education information
+var education = {
+	"schools": [
+		{
+			"name": "Udacity",
+			"degree": "Nanodegree",
+			"majors": "Front End Web Design",			
+			"dates": "May 2015",
+			"location": "Online",
+			"url": "http://udacity.com"
+		},
+		{
+			"name": "University of Michigan",
+			"degree": "Bachelor of Science",
+			"majors": "Electrical Engineering",			
+			"dates": "May 1995",
+			"location": "Ann Arbor, MI",
+			"url": "http://umich.edu"
+		},
+		{
+			"name": "Nouvel Catholic Central High School",
+			"degree": "",
+			"majors": "",
+			"dates": "May 1990",
+			"location": "Saginaw, MI",
+			"url": "http://www.sacschools.org/index.php/our-schools/nouvel-high-school-ncchs"	
+		}
+	],
+	"onlineCourses": [
+		{
+			"name": "Udacity",
+			"degree": "Nanodegree",
+			"majors": "Front End Web Design",
+			"dates": 2015,
+			"url": "http://udacity.com"
+		}
+	]
+};
+
+// resume certification information
+var certifications = {
+	"certs": [
+		{
+			"company": "Avaya",
+			"certname": [
+				"APDS (Avaya Professional Design Specialist) - Unified Communications (Current)",
+				"APDS (Avaya Professional Design Specialist) - Avaya Networking (Current)",
+				"APDS (Avaya Professional Design Specialist) - Contact Center (Current)",
+				"ACS (Avaya Certified Specialist) - IP Telephony",
+				"ACA (Avaya Certified Associate) - IP Telephony",
+				"NCDE (Nortel Certified Design Expert) - 3C, Wireless LAN & IP Convergence 25.40 Design Expert",
+				"NCDS (Nortel Certified Support Expert) - 3C",
+				"NCDS (Nortel Certified Design Specialist) - IP Enabled 25.40 & CS1000 (Rls 4.0 & 6.0)",
+				"NCSS (Nortel Certified Support Specialist) - Voice Solutions & CallPilot 2.0"
+			]
+		},
+		{
+			"company": "Cisco",
+			"certname": [
+				"CCDA (Cisco Certified Design Associate)",
+				"CCNA (Cisco Certified Network Associate)",
+				"CCSE (Cisco Certified Sales Expert)",
+				"Advanced Routing and Switching Specialization"
+			]
+		},
+		{
+			"company": "The SIP School",
+			"certname": [
+				"SSCA (SIP School Certified Associate) (Current)"
+			]
+		},
+		{
+			"company": "Microsoft",
+			"certname": [
+				"MCSE (Microsoft Certified Systems Engineer)",
+				"MCP (Microsoft Certified Professional)"
+			]
+		},
+		{
+			"company": "Juniper",
+			"certname": [
+				"JNSS-EN (Juniper Networks Sales Specialist - Enterprise Networking"
+			]
+		},
+		{
+			"company": "Adtran",
+			"certname": [
+				"ATSA (Adtran Telephony Support Specialist) - IP Telephony"
+			]
+		}
+	]
+};
+
+//HEADER SECTION//
+// code formats the header section; uses helper.js
+var formattedName = HTMLheaderName.replace("%data%",bio.name);  //format the name from helper.js
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);  //format the role from helper.js
+var formattedPic = HTMLbioPic.replace("%data%",bio.bioPic);  //insert the picture location into helper.js
+var formattedWelcome = HTMLWelcomeMsg.replace("%data%",bio.welcomemsg);  //format the welcome message from helper.js
+formattedRole = formattedRole.replace("<span>", "<span class=\"white-text\">");  //change class on formatted role
+$("#header").prepend(formattedName,formattedRole);  //prepend the name and role to the header id element
+$("#header").append(formattedPic);  //append the picture to the header id element
+$("#header").append(formattedWelcome);  //append the welcome message to the hdaer id element
+// format and appen the skills to the hedaer id element
+if (bio.skills.length !== 0) { //if the skills object does not have a length of zero, do the following
+	$("#header").append(HTMLskillsStart);  //append the header id element with the skill start format from helper.js
+	
+	for (i in bio.skills) {  //step through the skills list formatting it using the helper.js template and appending it to the skills id element
+		var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
+		$("#skills").append(formattedSkills);
+	};
+};
+// add a character section along with character attributes
+if (bio.character.length !== 0) {  //if the character object does not have a length of zero, do the following
+	var formattedCharacter = HTMLskillsStart.replace("Skills at a Glance", "Character Traits");  //format the character title from the skills start helper.js template
+	formattedCharacter = formattedCharacter.replace("id=\"skillsH3", "id=\"traitsH3");  //change the skillsH3 id to traitsH3 for this new element
+	formattedCharacter = formattedCharacter.replace("id=\"skills", "id=\"traits");  //change the skills id to the traits id for this new element
+	$("#header").append(formattedCharacter);  //add this new element to the header id
+	$("#traitsH3").css("color", "#F5AE23");  //set the color of the element traitsH3
+	
+	for (i in bio.character) {  //step through the character list formatting it using the helper.js template an appending it to the traits id element
+		var formattedTraits = HTMLskills.replace("%data%",bio.character[i]);
+		$("#traits").append(formattedTraits);
+	};
+};
+$("#header").css("background-color", "saddlebrown");  //change the background color of the header section to saddlebrown
+$("#footerContacts").css("background-color", "saddlebrown");  //change the background color of the footer section to saddlebrown
+
+//WORK EXPERIENCE SECTION//
+//Code formats the work experience section
+function displayWork(){  //function walks through the work.jobs object to create the work experience section
+	for (j in work.jobs) {  //loops through work.jobs to place the information in the work experience section
+		//sets up the header for the job, formats the employer and title, and appends it to the work-entry class
+		$("#workExperience").append(HTMLworkStart);
+		var formattedWorkEmployer = HTMLworkEmployer.replace("%data%",work.jobs[j].employer);
+		var formattedWorkTitle = HTMLworkTitle.replace("%data%",work.jobs[j].title);
+		$(".work-entry:last").append(formattedWorkEmployer+formattedWorkTitle);
+
+		//formats the location and appends to work-entry class
+		var formattedWorkLocation = HTMLworkLocation.replace("%data%",work.jobs[j].location);
+		$(".work-entry:last").append(formattedWorkLocation);
+
+		//formats the dates and appends to work-entry class
+		var formattedWorkDates = HTMLworkDates.replace("%data%",work.jobs[j].dates);
+		$(".work-entry:last").append(formattedWorkDates);
+
+		//formats the description and appends to the work-entry class
+		var formattedWorkDescription = HTMLworkDescription.replace("%data%",work.jobs[j].description);
+		$(".work-entry:last").append(formattedWorkDescription);
+
+		//formatts the work experience bullets and appends them to the work-entry class as bullets
+		if (work.jobs[j].experience.length > 0) {
+			for (m in work.jobs[j].experience) {
+				$(".work-entry:last").append("<ul>");
+				$(".work-entry:last").append("<li>" +  "<p>"+ work.jobs[j].experience[m] + "</p>" + "</li>");
+				$(".work-entry:last").append("</ul>");
+			};
+		};
+		$(".work-entry:last").append("<br>");  //add line feed
+	};
+};
+//calls the work experience function above
+displayWork();
+
+//PROJECTS SECTION//
+//Code formats the projects section
+projects.display = function() {  //function walks through the projects.projects object to create the projects section
+	for (k in projects.projects) {  //loops through projects.projects to place the information in the projects section
+		//sets up the header for the project, formats the title, and appends it to the project-entry class
+		$("#projects").append(HTMLprojectStart);
+		var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.projects[k].title);
+		$(".project-entry:last").append(formattedProjectTitle);
+
+		//formats the dates and appends to project-entry class
+		var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.projects[k].dates);
+		$(".project-entry:last").append(formattedProjectDates);
+
+		//formats the description and appends to project-entry class
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.projects[k].description);
+		$(".project-entry:last").append(formattedProjectDescription);
+
+		//formats the impage and appends to project-entry class
+		var formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[k].images);
+		$(".project-entry:last").append(formattedProjectImage);
+	};
+};
+//calls the projects function above
+projects.display();
+
+//EDUCATION SECTION//
+//Code formats the education section
+education.display = function() {   //function walks through the education.schools object to create the education section
+	for (k in education.schools) {  //loops through education.schools to place the information in the education section
+		//sets up the header for the school, formats the name, url and degree, and appends it to the education-entry class
+		$("#education").append(HTMLschoolStart);
+		var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[k].name);
+		var formattedSchoolName = formattedSchoolName.replace("#",education.schools[k].url);
+		if (education.schools[k].degree === "") {  //determine if a degree is empty; if so, format degree to empty, else format the degree in the object
+			var formattedSchoolDegree = "";
+		} else {
+			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[k].degree);
+		};
+		$(".education-entry:last").append(formattedSchoolName+formattedSchoolDegree);
+
+		//formats the dates and appends to education-entry class
+		var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[k].dates);
+		$(".education-entry:last").append(formattedSchoolDates);
+
+		//formats the location and appends to education-entry class
+		var formattedSchoolLocs = HTMLschoolLocation.replace("%data%",education.schools[k].location);
+		$(".education-entry:last").append(formattedSchoolLocs);
+
+		//formats the major (if one exists) and appends to education-entry class
+		if (education.schools[k].majors === "") {
+			var formattedSchoolMajor= HTMLschoolMajor.replace("Major: %data%",education.schools[k].majors);;
+		} else {
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%",education.schools[k].majors);
+		};
+		$(".education-entry:last").append(formattedSchoolMajor);
+	};
+};
+//calls the education function above
+education.display();
+
+//CERTIFICATIONS SECTION//
+//Code formats the certification section
+$("<div id='certs'></div>").insertAfter("#education");  //inserts a certs element
+$("#certs").append("<h2>Certifications</h2>");  //inserts a Certifications header
+function displayCerts(){  //function walks through the certifications.certs object to create the certifications section
+	for (n in certifications.certs) {  //loops through certifications.certs to place the information in the certifications section
+		//sets up the header for the cert, formats the company, and appends it to the work-entry class
+		$("#certs").append(HTMLworkStart);
+		var formattedCertCompany = HTMLworkEmployer.replace("%data%",certifications.certs[n].company);
+		$(".work-entry:last").append(formattedCertCompany);
+
+		//formats the certification and appends to work-entry class
+		$("#certs").css("list-style", "square");
+		if (certifications.certs[n].certname.length > 0) {
+			for (o in certifications.certs[n].certname) {
+				$(".work-entry:last").append("<p>" + certifications.certs[n].certname[o] + "</p>");
+			};
+		};
+		$(".work-entry:last").append("<br>");  //line feed
+	};
+};
+//calls the certification function ablove
+displayCerts();
+
+
+//following small section of code used in Javascript class; not used for the resume
+function inName(name){
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	return name[0] +" "+name[1];
+};
+
+//$("#main").append(internationalizeButton);
+
+//enables the googlemap and sets the background color to gray since an additional section was added above it
+$("#mapDiv").addClass("gray");
+$("#mapDiv").append(googleMap);
+
+//add contact information to the footer section
+var formattedContacts = HTMLskills.replace("%data%","Mobile: " + bio.contacts.mobile);
+$("#footerContacts").append(formattedContacts);
+var formattedContacts = HTMLskills.replace("%data%","Email: " + bio.contacts.email);
+$("#footerContacts").append(formattedContacts);
+var formattedContacts = HTMLskills.replace("%data%","Twitter: " + bio.contacts.twitter);
+$("#footerContacts").append(formattedContacts);
